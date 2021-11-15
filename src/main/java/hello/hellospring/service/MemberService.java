38c13da80c0588914c463077349d854306d4bbe9
jinyoungk1208 <jinyoungk1208@gmail.com>
2021-11-15 22:28:56 +0900

    회원가입 서비스 테스트, DI 입문

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,7 +9,11 @@ import java.util.Optional;
 
 public class MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    } // memberRepository를 직접 new로 생성하는 것이 아니라 외부에서 주입하도록 메소드 생성 = DI(Dependency Injection) 의존성 주입
 
     /* 회원 가입 */
     public Long join(Member member) {
